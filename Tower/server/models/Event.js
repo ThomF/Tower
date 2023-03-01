b6db000b3db1c913dd169590b00c6c0da5854334
ThomF <seniorswanky@icloud.com>
2023-03-01 09:35:20 -0700

    POST create event done

--- Tower/server/models/Event.js
+++ Tower/server/models/Event.js
@@ -9,8 +9,9 @@ export const EventSchema = new Schema({
     location: { type: String, maxLength: 500, required: true },
     capacity: { type: Number, maxLength: 300, required: true },
     startDate: { type: String, maxLength: 300, required: true },
-    isCancelled: { type: Boolean, default: false, required: true },
-    coverImg: { type: String, maxLength: 5000 }
+    isCanceled: { type: Boolean, default: false, required: true },
+    coverImg: { type: String, maxLength: 5000 },
+    type: { ...basicStringType, required: true }
 
 }, { timestamps: true, toJSON: { virtuals: true } })
 
